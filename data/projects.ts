8c34bf44694f0597e33998c9bd1e7ca4aa8139b0
Ilya <i.a.4esnokov@gmail.com>
2026-09-18 13:05:04 +0300

    fix: swapped the cases

--- data/projects.ts
+++ data/projects.ts
@@ -2,6 +2,15 @@ import { ProjectDetails, ProjectPreview } from "@/types/project";
 
 export const projectsPreviewData: ProjectPreview[] = [
     {
+        id: "moerodnoe",
+        title: "Moerodnoe",
+        type: "Personal Website",
+        format: "landscape",
+        img: "/project-cases/moerodnoe/project-case-moerodnoe0.avif",
+        hoverImg: "/project-cases/moerodnoe/project-case-moerodnoe1.avif",
+        span: 1,
+    },
+    {
         id: "two_pistolets",
         title: "Two-Pistolets",
         type: "TG Mini App",
@@ -17,38 +26,7 @@ export const projectsPreviewData: ProjectPreview[] = [
         type: "Startup Marketplace",
         format: "landscape",
         img: "/project-cases/code-venture/project-case-code-venture0.avif",
-        hoverImg:
-            "/project-cases/code-venture/project-case-code-venture1.avif",
-        span: 1,
-    },
-    {
-        id: "pashalka",
-        title: "Pashalka",
-        type: "TG Mini App",
-        format: "portrait",
-        img: "/project-cases/pashalka/project-case-pashalka0.avif",
-        hoverImg:
-            "/project-cases/pashalka/project-case-pashalka1.avif",
-        span: 1,
-    },
-    {
-        id: "upora",
-        title: "Upora",
-        type: "Digital Products Marketplace",
-        format: "landscape",
-        img: "/project-cases/upora/project-case-upora0.avif",
-        hoverImg:
-            "/project-cases/upora/project-case-upora1.avif",
-        span: 1,
-    },
-    {
-        id: "moerodnoe",
-        title: "Moerodnoe",
-        type: "Personal Website",
-        format: "landscape",
-       img: "/project-cases/moerodnoe/project-case-moerodnoe0.avif",
-        hoverImg:
-            "/project-cases/moerodnoe/project-case-moerodnoe1.avif",
+        hoverImg: "/project-cases/code-venture/project-case-code-venture1.avif",
         span: 1,
     },
     {
@@ -57,8 +35,25 @@ export const projectsPreviewData: ProjectPreview[] = [
         type: "Skin Trading Platforme",
         format: "landscape",
         img: "/project-cases/skin-space/project-case-skin-space0.avif",
-        hoverImg:
-            "/project-cases/skin-space/project-case-skin-space1.avif",
+        hoverImg: "/project-cases/skin-space/project-case-skin-space1.avif",
+        span: 1,
+    },
+    {
+        id: "pashalka",
+        title: "Pashalka",
+        type: "TG Mini App",
+        format: "portrait",
+        img: "/project-cases/pashalka/project-case-pashalka0.avif",
+        hoverImg: "/project-cases/pashalka/project-case-pashalka1.avif",
+        span: 1,
+    },
+    {
+        id: "upora",
+        title: "Upora",
+        type: "Digital Products Marketplace",
+        format: "landscape",
+        img: "/project-cases/upora/project-case-upora0.avif",
+        hoverImg: "/project-cases/upora/project-case-upora1.avif",
         span: 1,
     },
 ];
